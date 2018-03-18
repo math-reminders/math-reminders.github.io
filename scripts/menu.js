44d38cf6124dcac36c794d2f9d5a3c219e81b8ac
Vadim <vadimkholodilo@gmail.com>
2018-03-18 18:33:26 +0300

    Now equations are rendering currectly on every page

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,18 +3,21 @@ $("#about-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/about.htm");
     history.pushState(null, null, "/about.htm");
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
     );
     $("#algebra-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/algebra.htm");
     history.pushState(null, null, "algebra.htm");
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     }
     );
 $("#geometry-page").click(function(e) {
 e.preventDefault();
     $("#main-content").load("/geometry.htm");
     history.pushState(null, null, "/geometry.htm");
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     }
     );
 }
