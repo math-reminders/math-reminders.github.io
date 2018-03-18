354828c269ba1d5ede3a7c22a5020555d4f3131a
Vadim <vadimkholodilo@gmail.com>
2018-03-18 18:41:34 +0300

    mend

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,21 +3,18 @@ $("#about-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/about.htm");
     history.pushState(null, null, "/about.htm");
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
     );
     $("#algebra-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/algebra.htm");
     history.pushState(null, null, "algebra.htm");
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     }
     );
 $("#geometry-page").click(function(e) {
 e.preventDefault();
     $("#main-content").load("/geometry.htm");
-    history.pushState(null, null, "/geometry.htm");
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+history.pushState(null, null, "/geometry.htm");
     }
     );
 }
