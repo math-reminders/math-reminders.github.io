dc430449759b4619439f3f6c44f198a0e138475b
Vadim <vadimkholodilo@gmail.com>
2018-03-18 18:11:35 +0300

    Now ajax script updates adress bar

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,16 +2,19 @@ function menu() {
 $("#about-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/about.htm");
+    history.pushState(null, null, "/about.htm");
 }
     );
     $("#algebra-page").click(function(e) {
     e.preventDefault();
     $("#main-content").load("/algebra.htm");
+    history.pushState(null, null, "algebra.htm");
     }
     );
 $("#geometry-page").click(function(e) {
 e.preventDefault();
     $("#main-content").load("/geometry.htm");
+    history.pushState(null, null, "/geometry.htm");
     }
     );
 }
